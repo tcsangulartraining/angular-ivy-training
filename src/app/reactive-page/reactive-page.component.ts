621c74d49a2f438f3662d500bf6adda3acd44253
tcsangulartraining <tcsangulartraining@gmail.com>
2022-02-14 19:42:32 +0530

    1 file modified, 1 added

--- src/app/reactive-page/reactive-page.component.ts
+++ src/app/reactive-page/reactive-page.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+  MinLengthValidator,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-page',
   templateUrl: './reactive-page.component.html',
-  styleUrls: ['./reactive-page.component.css']
+  styleUrls: ['./reactive-page.component.css'],
 })
 export class ReactivePageComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
+  registrationForm = this.fb.group({
+    userName: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+    city: ['', Validators.required],
+  });
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  onSubmit() {
+    console.log(this.registrationForm.value);
   }
-
 }
