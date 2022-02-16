02386ca15c16ff28cb3068d69f87fa2c35636a82
tcsangulartraining <tcsangulartraining@gmail.com>
2022-02-16 15:44:40 +0530

    1 file modified, 1 added

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+  MinLengthValidator,
+} from '@angular/forms';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  styleUrls: ['./employee.component.css'],
 })
 export class EmployeeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {}
+  //employeeForm;
+  employeeForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', Validators.required],
+    city: ['', Validators.required],
+    state: ['', Validators.required],
+  });
+  ngOnInit() {}
+  onSubmit() {
+    console.log(this.employeeForm.value);
   }
-
 }
