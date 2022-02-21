fd97693ce9124475bc98907cce5233c72d28bd7f
tcsangulartraining <tcsangulartraining@gmail.com>
2022-02-21 19:48:32 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule , ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
+  declarations: [ AppComponent, HelloComponent, EmployeeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
