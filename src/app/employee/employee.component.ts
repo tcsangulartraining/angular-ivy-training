bdfcc8ce001831b6233fde9afe69a0afa6fcd76c
tcsangulartraining <tcsangulartraining@gmail.com>
2022-02-22 10:39:05 +0530

    5 files modified, 2 added

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -7,15 +7,21 @@ import {
   MinLengthValidator,
 } from '@angular/forms';
 import { Store } from '@ngrx/store/src';
+import { Employee } from '../shared/employee';
+import { addEmployee, viewEmployee } from '../state/employee/employee.actions';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css'],
 })
 export class EmployeeComponent implements OnInit {
-  constructor(private fb: FormBuilder, private store: Store) {}
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<{ employee: { employee: Employee } }>
+  ) {}
   //employeeForm;
-  employeeForm = this.fb.group({
+  //employee: FormGroup;
+  employee = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(5)]],
     email: ['', Validators.required],
     city: ['', Validators.required],
@@ -23,6 +29,8 @@ export class EmployeeComponent implements OnInit {
   });
   ngOnInit() {}
   onSubmit() {
-    console.log(this.employeeForm.value);
+    this.store.dispatch(addEmployee());
+
+    console.log(this.store.select(viewEmployee));
   }
 }
