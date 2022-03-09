063a29915544fbbde2fa9a6cddaad54aee37e839
Ash <Ash123@.com>
2022-03-09 16:34:55 +0530

    Implementation of auth Effects

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule} from '@ngrx/effects';
 import { LoginComponent } from './login/login.component';
+import { AuthEffects } from './state/auth.effects';
 import { AuthReducer } from './state/auth.reducer';
 import { AUTH_STATE_NAME } from './state/auth.selector';
 
@@ -14,8 +16,13 @@ const routes: Routes=[
     ]}
 ]
 @NgModule({
-    imports:[CommonModule, FormsModule,
-        ReactiveFormsModule,StoreModule.forFeature(AUTH_STATE_NAME, AuthReducer),RouterModule.forChild(routes)],
+    imports:[
+        CommonModule, 
+        FormsModule,ReactiveFormsModule,
+        EffectsModule.forFeature([AuthEffects]),
+        StoreModule.forFeature(AUTH_STATE_NAME, AuthReducer),
+        RouterModule.forChild(routes)
+    ],
     declarations:[LoginComponent]
 })
 export class AuthModule{}
--- src/app/auth/state/auth.effects.ts
+++ src/app/auth/state/auth.effects.ts
@@ -2,27 +2,22 @@ import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { AuthService } from 'src/app/services/auth.service';
 import { loginStart, loginSuccess } from './auth.actions';
-import { exhaustMap, map} from 'rxjs/operators'
-import {
-    Observable,
-    pipe
-  } from 'rxjs';
+import { exhaustMap, map, mergeMap} from 'rxjs/operators'
+
 
 @Injectable()
 
 export class AuthEffects{
     constructor(private actions$:Actions, private authService:AuthService){}
     
-    login$ = createEffect(()=>{
-        return this.actions$.pipe(
+    login$ = createEffect(()=>
+        this.actions$.pipe(
             ofType(loginStart),
-            exhaustMap((action)=>{
-                this.authService.login(action.email, action.password).pipe(
-                    map((data)=>{
-                        return loginSuccess();
-                    })
-                )
-            })
+            mergeMap((action: { email: string; password: string; })=>this.authService.login(action.email, action.password)
+            .pipe(
+                map(data=>{return loginSuccess();})
+            ))
         )
-    })
+    )
+        
 }
